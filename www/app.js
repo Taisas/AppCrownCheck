cb617ac6088f96fd117f5118fd6c471c4f44cc87
井指太一 <taichi@iyubitaichi-no-MacBook-Pro.local>
2016-11-24 22:04:51 +0900

    modfy

--- www/app.js
+++ www/app.js
@@ -2,6 +2,12 @@ ons.bootstrap()
     .controller('AppController', function() {
         this.pushes = 0;
         this.pops = 0;
+        this.menus = [
+            'ローマ皇帝',
+            'フランス王',
+            'イングランド王',
+            '神聖ローマ帝国皇帝'
+        ];
     });
 ons.ready(function() {
     console.log("Onsen UI is ready!");
