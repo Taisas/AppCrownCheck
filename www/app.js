52824cc91fa18165f690aac51a47b18a94ea6de3
Taisas <tisashi15@ad.aol.aoltw.net>
2016-11-25 12:28:28 +0900

    ページ遷移組み込み

--- www/app.js
+++ www/app.js
@@ -3,11 +3,14 @@ ons.bootstrap()
         this.pushes = 0;
         this.pops = 0;
         this.menus = [
-            'ローマ皇帝',
-            'フランス王',
-            'イングランド王',
-            '神聖ローマ帝国皇帝'
+            ['ローマ皇帝', 'a001'],
+            ['フランス王', 'a002'],
+            ['イングランド王', 'a003'],
+            ['神聖ローマ帝国皇帝', 'a004']
         ];
+        this.pushCrownPage = function (id) {
+            navi.pushPage(id + '.html');
+        }
     });
 ons.ready(function() {
     console.log("Onsen UI is ready!");
