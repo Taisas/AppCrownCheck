147963f134d1f50fbaf41b59257c1ba85c0fa701
Taisas <tisashi15@ad.aol.aoltw.net>
2016-12-16 16:23:53 +0900

    検索機能追加

--- www/app.js
+++ www/app.js
@@ -1,4 +1,4 @@
-ons.bootstrap()
+angular.module('myApp', ['onsen', 'ui.bootstrap'])
     .factory('JsonData', function ($http) {
         return{
             getJson : function () {
@@ -14,27 +14,56 @@ ons.bootstrap()
         };
     })
     .controller('AppController', function($scope, JsonData) {
-        this.name = '';
-        this.exp = '';
+        $scope.name = '';
+        $scope.exp = '';
+        $scope.keywd = '';
         $scope.menus = [];
         $scope.romans = [];
         $scope.francs = [];
         $scope.englands = [];
         $scope.holyromans = [];
+        $scope.names = [];
+        $scope.rexdata = {};
         JsonData.getJson().then(function(res){
             $scope.menus = res.data.menus;
             $scope.romans = res.data.roma;
             $scope.francs = res.data.france;
             $scope.englands = res.data.england;
             $scope.holyromans = res.data.holyroma;
+            $scope.romans.forEach(function (v, i) {
+                $scope.names.push(v.name);
+                $scope.rexdata[v.name ]= v.exp;
+            });
+            $scope.francs.forEach(function (v, i) {
+                $scope.names.push(v.name);
+                $scope.rexdata[v.name ]= v.exp;
+            });
+            $scope.englands.forEach(function (v, i) {
+                $scope.names.push(v.name);
+                $scope.rexdata[v.name ]= v.exp;
+            });
+            $scope.holyromans.forEach(function (v, i) {
+                $scope.names.push(v.name);
+                $scope.rexdata[v.name ]= v.exp;
+            });
         });
-        this.pushCrownPage = function (id) {
+        $scope.pushCrownPage = function (id) {
             navi.pushPage(id + '.html', { animation: "slide" });
         };
-        this.showDedtail = function(name, exp) {
-            this.name = name;
-            this.exp = exp;
+        $scope.showDedtail = function(name, exp) {
+            $scope.name = name;
+            $scope.exp = exp;
             navi.pushPage('detail.html', { animation: "slide" });
+        };
+        $scope.forwardMatch = function(value, input) {
+            return value.toLowerCase().indexOf(input.toLowerCase()) === 0;
+        };
+        $scope.rexserch = function(key){
+            $scope.name = key;
+            $scope.exp = $scope.rexdata[key];
+            navi.pushPage('detail.html', { animation: "slide" });
+            // console.log($scope.rexdata[key]);
+
         };
     });
 ons.ready(function() {
